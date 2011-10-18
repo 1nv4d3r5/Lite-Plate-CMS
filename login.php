8f0442993967ddb872072566b170fbee600a3ff5
Scott Hutchinson <liteplate@gmail.com>
2011-10-18 00:51:23 -0500

    v0.3.1-2; stable, incomplete

--- login.php
+++
@@ -1,9 +0,0 @@
-<?php
-ini_set('display_errors',1);
-error_reporting(E_ALL | E_STRICT);
-include_once('/var/www/inc/constants.php');
-include(DOC_ROOT . 'inc/Page.php');
-$page = new Page('Sign In', 'login', 'inc/content/login_content.php', 'inc/content/login_subcontent.php', 'script/form_scripts_min.php');
-$page->display();
-unset($page);
-?>
