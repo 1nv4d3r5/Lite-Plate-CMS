bc73e9fa3df69bb3265d2548168fa8b287133667
Scott Hutchinson <liteplate@gmail.com>
2011-10-26 18:25:12 -0500

    fix

--- script/form_scripts_min.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once '/var/www/inc/constants.php';
-include_once DOC_ROOT . 'inc/compress_file.php';
-compress_file(array(DOC_ROOT . 'script/form_scripts.js'), 'text/javascript', true, 345600);
-?>
--- script/return_to_top_min.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once '/var/www/inc/constants.php';
-include_once DOC_ROOT . 'inc/compress_file.php';
-compress_file(array(DOC_ROOT . 'script/return_to_top.js'), 'text/javascript', true, 345600);
-?>
